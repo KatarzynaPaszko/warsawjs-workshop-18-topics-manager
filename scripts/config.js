e69d4c591cacfadff33d008b6179c9c1d103ebf8
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2018-03-18 18:04:51 +0100

    udało się xD

---
+++ scripts/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    GITHUB: {
+        CLIENT_ID: `b1cb70b1910ce5051fe5`
+    }
+};
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
 require ('bulma');
+
+const GITHUB = require('./config').GITHUB;
 const hello = require('hellojs');
+const topics = new Set();
+const $login = document.querySelector(".jslog");
+const $logout = document.querySelector(".jslogout");
+const $jsnav = document.querySelector(".add");
+const $jstopics = document.querySelector(".jstopics");
 
 if ( hello('github').getAuthResponse() != null ) {
-  console.log("true");
   renderUser();
 }
 
@@ -10,15 +16,40 @@ hello.init({
   github: 'b1cb70b1910ce5051fe5'
 });
 
+const $form = document.querySelector('.js-form-add-topic');
+$form.addEventListener("submit", (e)=> {
+  e.preventDefault();
+  const data = new FormData($form);
+  const map = new Map(data.entries());
+  console.log(map);
 
+  $form.reset();
+  renderTopics(map)
+})
 
-const $login = document.querySelector(".jslog");
-const $logout = document.querySelector(".jslogout");
-const $jsnav = document.querySelector(".add");
-
-console.log( hello('github').getAuthResponse() );
-
-
+function renderTopic(mapka) {
+  const template2 = `
+    <div class="column is-3">
+        <div class="card">
+            <header class="card-header">
+                <p class="card-header-title">
+                    ${mapka.get("topic")}
+                </p>
+            </header>
+            <div class="card-content">
+                <div class="content">
+                  ${mapka.get("description")}
+                </div>
+            </div>
+            <footer class="card-footer">
+                <a href="#" class="card-footer-item">Zagłosuj</a>
+                <a href="#" class="card-footer-item">Chcę być trenerem</a>
+            </footer>
+        </div>
+    </div>
+  `;
+  $jstopics.innerHTML += template2;
+}
 
 $login.addEventListener("click", (e) => {
   if ( hello('github').getAuthResponse() != null ) {
@@ -27,7 +58,6 @@ $login.addEventListener("click", (e) => {
   e.preventDefault();
   hello('github').login()
     .then(renderUser);
-
 });
 
 function renderUser(){
