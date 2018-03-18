2ec7139418e70d0498260e49c9c670cfe06ba507
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2018-03-18 17:02:03 +0100

    dziala xD

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,60 @@
 require ('bulma');
 const hello = require('hellojs');
 
+if ( hello('github').getAuthResponse() != null ) {
+  console.log("true");
+  renderUser();
+}
+
 hello.init({
   github: 'b1cb70b1910ce5051fe5'
 });
 
+
+
 const $login = document.querySelector(".jslog");
 const $logout = document.querySelector(".jslogout");
-const $jsnav = document.querySelector(".jsnav");
+const $jsnav = document.querySelector(".add");
+
+console.log( hello('github').getAuthResponse() );
+
 
 
 $login.addEventListener("click", (e) => {
-  console.log("1");
+  if ( hello('github').getAuthResponse() != null ) {
+    return;
+  }
   e.preventDefault();
   hello('github').login()
-  .then(function(){
-    console.log("2");
-    return hello('github').api('/me');
-  })
+    .then(renderUser);
+
+});
+
+function renderUser(){
+
+
+  hello('github').api('/me')
   .then(function(userProfile){
     const Profile = {
       avatar: userProfile.avatar_url,
-      loginek: userProfile.login
+      login: userProfile.login
     }
     renderUserDetails(Profile);
   })
-});
-function renderUserDetails (Profile) {
+}
 
+$logout.addEventListener("click", (e) => {
+  hello.logout('github')
+    .then(() => location.reload());
+});
+
+
+function renderUserDetails (Profile) {
   const template = `
     <div  class="navbar-item">
-      ${Profile.loginek}
+      <h5>${Profile.login}</h5>&nbsp;
       <img src="${Profile.avatar}" alt="user avatar"/>
     </div>
   `;
   $jsnav.innerHTML += template;
-
 }
-
-$logout.addEventListener("click", (e) => {
-  e.preventDefault();
-  hello.logout('github');
-});
